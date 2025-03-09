9be9b493525d369b449469179f387ad7c5e4f775
unknown <diogosilvabianchini@gmail.com>
2025-03-09 19:47:53 -0300

    feat: Setup inicial do banco de dados.

---
+++ database/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+
+const {DATABASE_USER, DATABASE_PASS, DATABASE_NAME, DATABASE_HOST} = process.env
+
+module.exports = {
+  "development": {
+    "username": DATABASE_USER,
+    "password": DATABASE_PASS,
+    "database": DATABASE_NAME,
+    "host": DATABASE_HOST,
+    "dialect": "postgres"
+  },
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // }
+}
