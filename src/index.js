e10ffc7d1a2fb9020fcb3d9bb618f4a9182a57a5
unknown <diogosilvabianchini@gmail.com>
2025-03-09 21:07:31 -0300

    feat: conexão node e banco dedos.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const routes = require("./routes/routes")
+const {sequelize} = require("../database/models")
 
 const app = express()
 const PORT = process.env.PORT || 8082
+
+sequelize.sync({alter: true}).then(() => {
+    console.log("Banco de dados ativo")
+}).catch(err => {
+    console.error(err)
+})
 
 app.use(morgan("dev"))
 app.use(routes)
